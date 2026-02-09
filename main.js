3d0815313877218a2be9138cd4e8414a2e405835
MOE-dot-1127 <moyashi1127@outlook.jp>
2026-02-10 02:35:56 +0900

    Add floor and collision detection

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ scene.add(light, new THREE.AmbientLight(0xffffff, 0.7), new THREE.GridHelper(200
 const mazeLoader = new GLTFLoader();
 mazeLoader.load('/models/maze.glb', (gltf) => {
     const maze = gltf.scene;
-    maze.position.set(0, 20,);
+    maze.position.set(0,10,0);
     scene.add(maze);
 
     maze.traverse((child) => {
